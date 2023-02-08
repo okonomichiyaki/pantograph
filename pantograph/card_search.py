b01ee3e8bda5a0cdbcace7ad4df05ddda2efd914
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2023-02-08 09:45:47 -0500

    delete opencv stuff

--- pantograph/card_search.py
+++ pantograph/card_search.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import logging
-import imagehash
 import time
 import sys
 
@@ -9,8 +8,6 @@ from pathlib import Path
 from rapidfuzz import process
 from rapidfuzz.distance.Levenshtein import distance
 from dataclasses import dataclass
-from PIL import Image
-import numpy as np
 
 from pantograph.nrdb import get_active_cards
 
@@ -34,7 +31,7 @@ def fuzzy_search(text, titles, cards):
 def fuzzy_search_multiple(texts, titles, cards):
     results = [_extract(text, titles) for text in texts]
     results = [result[0] for result in results]
-    results = sorted(results, key=lambda result: result[0])
+    results = sorted(results, key=lambda result: result[0]) # TODO: shouldn't this key on result[1] ?
     if len(results) > 0:
         title = results[0][0]
         card = cards[title]
@@ -43,74 +40,10 @@ def fuzzy_search_multiple(texts, titles, cards):
     else:
         return None
 
-def combined_hash(img):
-    p = imagehash.phash(img)
-    c = imagehash.colorhash(img)
-    return (p,c)
-
-def diff_hash(a, b):
-    pdiff = a[0] - b[0]
-    cdiff = a[1] - b[1]
-    return pdiff + cdiff
-
-def build_image_hash_db(imagesdir, cards, hashfunc=imagehash.phash):
-    start = time.perf_counter()
-
-    path = Path(imagesdir)
-    if '~' in imagesdir:
-        path = path.expanduser()
-    filenames = path.glob('**/*.jpg')
-    hashes = []
-    for f in sorted(filenames):
-        try:
-            hash = hashfunc(Image.open(f))
-            code = int(f.name[0:-4])
-            data = cards[code]
-            hashes.append((hash, data))
-        except Exception as e:
-            logging.error(f"Caught exception computing hash for {f}: ", e)
-            continue
-
-    elapsed = time.perf_counter() - start
-    logger.info(f"built hash database in {elapsed}")
-    return hashes
-
-def search_by_image_hash(imgpath, hashes, hashfunc=imagehash.phash):
-    start = time.perf_counter()
-
-    target = hashfunc(Image.open(imgpath))
-    results = sorted(hashes, key=lambda h: diff_hash(target, h[0]))
-    best = results[:5]
-    best = [ b[1].title for b in best ]
-    logger.debug(f"best results: {best}")
-
-    return results[0][1]
-    # closest = (np.inf, None)
-    # for hash in hashes:
-    #     other = hash[0]
-    #     data = hash[1]
-    #     diff = other - target
-    #     if diff < closest[0]:
-    #         closest = (diff, data)
-
-    # elapsed = time.perf_counter() - start
-    # logger.debug(f"found closest {closest} in {elapsed}")
-    # return closest[1]
-
 class CardSearch:
-    def __init__(self, cards, imgdir, hashfunc=combined_hash):
+    def __init__(self, cards):
         self.cards = cards
         self.titles = [ card.get_simple_title() for card in cards.values() ]
-        if imgdir:
-            self.hashes = build_image_hash_db(imgdir, cards, hashfunc=hashfunc)
-            self._hashfunc = hashfunc
-
-    def image_search(self, imgpath):
-        if self.hashes:
-            return search_by_image_hash(imgpath, self.hashes, hashfunc=self._hashfunc)
-        else:
-            logger.error("No image hash database created")
-            return None
 
     def text_search(self, text):
         return fuzzy_search(text, self.titles, self.cards)
@@ -118,7 +51,6 @@ class CardSearch:
     def text_search_multiple(self, texts):
         return fuzzy_search_multiple(texts, self.titles, self.cards)
 
-# TODO: NRDB API code take data dir param
-def init_card_search(datadir=None, imagesdir=None):
+def init_card_search():
     cards = get_active_cards()
-    return CardSearch(cards, imagesdir)
+    return CardSearch(cards)
--- pantograph/cli.py
+++ pantograph/cli.py
@@ -1,11 +1,5 @@
-from __future__ import print_function
-import cv2 as cv
-import numpy as np
 import argparse
 import logging
-
-from io import StringIO
-from csv import DictReader
 
 import io
 import os
@@ -14,24 +8,10 @@ from itertools import groupby
 
 from pantograph.google_vision import recognize
 from pantograph.card_search import init_card_search
-from pantograph.loop import loop
-from pantograph.detect import Detector
-import pantograph.web as web
-
 
 def main():
     parser = argparse.ArgumentParser(description='TODO')
     parser.add_argument('--log-level', type=str, default='info')
-    parser.add_argument('--web', action='store_true', help='Start the web server.')
-    parser.add_argument(
-        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
-    )
-    parser.add_argument(
-        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
-    )
-    parser.add_argument(
-        "--camera", type=int, default=0, help="Camera device number"
-    )
     parser.add_argument(
         "--fuzzy", type=str, help="Fuzzy text search"
     )
@@ -41,15 +21,12 @@ def main():
 
     args = parser.parse_args()
 
-    #logging.basicConfig(
-    #filename='/var/log/pantograph/pantograph.log', encoding='utf-8', level=logging.DEBUG)
     if args.log_level.lower() == 'debug':
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
 
-    # TODO: config for images dir
-    card_search = init_card_search(imagesdir=None) #'/Users/aki/Downloads/images')
+    card_search = init_card_search()
 
     if (args.google_vision):
         filename = args.google_vision
@@ -63,15 +40,3 @@ def main():
         card = card_search.text_search(args.fuzzy)
         print(f"{card.title} [{card.code}]")
         exit(0)
-
-    backsub = cv.createBackgroundSubtractorMOG2()
-    detector = Detector(backsub, card_search)
-
-    if args.web:
-        web.run(args, detector)
-    else:
-        capture = cv.VideoCapture(args.camera)
-        if not capture.isOpened():
-            print("Cannot open camera")
-            exit()
-        loop(detector, capture)
