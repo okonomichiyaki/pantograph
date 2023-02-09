7a58f685a72351a4edc474a0871045623f387d8c
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2023-02-08 21:09:39 -0500

    fix entrypoint.sh for websockets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-poetry run gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 "pantograph.flask:create_app()"
+poetry run gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker pantograph.flask:app 
