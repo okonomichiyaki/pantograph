375d39b597f7986c75cd5719390f0116b3f4e47b
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2023-08-05 17:37:39 -0400

    wait for videos to end in cypress tests
    waiting for "should have css width 1280px" did not seem to wait properly. video was not even started and click missed the targets
    waiting for videos to end works, clicks hit targets now

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -2,8 +2,8 @@ describe('card search via click in demo mode', () => {
   it('passes', () => {
     cy.viewport(1920, 1080);
     cy.visit('http://localhost:8000/app/demo/?runner=esâ&corp=thule&local=0');
-    cy.get('#remote-video').should('have.prop', 'paused', false);
-    cy.get('#remote-video').should('have.css', 'width', '1280px');
+    cy.get('#remote-video').should('have.prop', 'ended', true);
+    cy.get('#local-video').should('have.prop', 'ended', true);
     cy.get('#remote-placeholder').should('not.be.visible');
     cy.get('#remote-video').clickByPercentage(915 / 1280, 615 / 720);
     cy.get('img[alt="Spin Doctor"]').should('be.visible');
